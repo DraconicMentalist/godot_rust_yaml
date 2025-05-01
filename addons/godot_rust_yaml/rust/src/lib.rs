db21630cced510253b299c3cb60dc9c963ee70b0
Bea <DraconicMentalist@gmail.com>
2025-05-01 00:57:53 -0400

    fixed mutability and other bugs keeping this from compiling

--- addons/godot_rust_yaml/rust/src/lib.rs
+++ addons/godot_rust_yaml/rust/src/lib.rs
@@ -1,5 +1,5 @@
 
-use godot::engine::Json;
+use godot::classes::Json;
 use godot::prelude::*;
 use godot::obj::cap::GodotDefault;
 use godot::builtin::GString;
@@ -22,12 +22,13 @@ impl YAML {
             Err(error) => return Variant::from(format!("Problem parsing the YAML: {:?}", error)),
         };
         let json_string = serde_json::to_string(&json_value).unwrap();
-        return Json::parse_string(GString::from(json_string));
+        return Json::parse_string(&GString::from(json_string));
     }
 
     #[func]
     fn to_string(input: Dictionary) -> GString {
-        let json_string = Json::stringify(Variant::from(input));
+        let input_string = &Variant::from(input);
+        let json_string = Json::stringify(input_string);
         let input: String = json_string.to_string();
         let json_parse_result: Result<serde_json::Value, serde_json::Error> = serde_json::from_str(&input);
         let yaml_value = match json_parse_result {
